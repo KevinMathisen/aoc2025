a1967dc13ecfbffc60218d0d2712a3fe7fcdb793
Kevin Nikolai Mathisen <kevin.nikolai.mathisen@gmail.com>
2025-12-20 15:32:07 +0100

    feat: day5 complete!

--- src/main/java/com/kevinnm/aoc/day5/Day5.java
+++ src/main/java/com/kevinnm/aoc/day5/Day5.java
@@ -49,18 +49,13 @@ public class Day5 implements Day {
         freshIds2 = new ArrayList<>(Arrays.asList(freshIds));
         freshIds2.sort((r1, r2) -> Long.compare(r1[0],r2[0]));
 
-        System.out.println("----- Fresh ids -----");
-        for (long[] range : freshIds2) {
-            System.out.println(Long.toString(range[0]) + " - " + Long.toString(range[1]));
-        }
-
         // merge each range with subsequent ranges such that
         //   the next range's start should be larger than current end
         int curIdx = 0;
         while (curIdx < freshIds2.size()-1) {
 
             // need to keep merging as long as current end is larger than next's start
-            while (curIdx < freshIds2.size()-1 && freshIds2.get(curIdx)[1] > freshIds2.get(curIdx+1)[0]) {
+            while (curIdx < freshIds2.size()-1 && freshIds2.get(curIdx)[1] >= freshIds2.get(curIdx+1)[0]) {
                 long[] curRange = freshIds2.get(curIdx);
 
                 // if range to merge with has larger end, set this
@@ -74,13 +69,7 @@ public class Day5 implements Day {
             curIdx++;
         }
 
-        System.out.println("----- Fresh ids -----");
-        for (long[] range : freshIds2) {
-            System.out.println(Long.toString(range[0]) + " - " + Long.toString(range[1]));
-        }
-
         // done with merge; have ranges with no overlap
-        // go through each range and add to total ids
         long totalIds = 0;
         for (long[] range : freshIds2) {
             totalIds += range[1] - range[0] + 1;   
@@ -88,78 +77,5 @@ public class Day5 implements Day {
 
         System.out.println("day 5 part 2: " + totalIds);
         return 0;
-    }
-
-    private void insertRangeIntoMerged(long[] rangeToInsert) {
-        long startInsert = rangeToInsert[0], endInsert = rangeToInsert[1];
-
-        int startInsertIndex = 0, endInsertIndex = 0;
-        while (startInsertIndex < freshIds2.size() 
-            && startInsert > freshIds2.get(startInsertIndex)[0]
-        ) startInsertIndex++; 
-
-        boolean isStartInsidePrevRange = (startInsertIndex > 0) && 
-                    startInsert <= freshIds2.get(startInsertIndex-1)[1];
-        if (isStartInsidePrevRange) startInsertIndex--;
-
-        while (endInsertIndex < freshIds2.size() 
-            && endInsert > freshIds2.get(endInsertIndex)[1]
-        ) endInsertIndex++; 
-
-        // e.g. 
-        // [1, 10], [12, 16]
-        // insert [12,14]
-        //          -> [1, 10], [12, 16]
-
-        // [1, 10], [12, 16]
-        // insert [12,18]
-        //          -> [1, 10], [12, 18]
-
-        // [5, 6], [8, 20], [21, 40]
-        // insert [10, 30]
-        //          -> [5, 6], [8, 40]
-
-        // [5, 6], [8, 20], [21, 40]
-        // insert [10, 50]
-        //          -> [5, 6], [8, 50]
-
-        // [5, 6], [8, 20], [21, 40]
-        // insert [45, 45]
-        //          -> [5, 6], [8, 20], [21, 40], [45, 45]
-
-        // need to find where to insert start, i.e. are we inside another range? between?
-        //  after this, we need to find where to insert end. 
-
-        // if any ranges between: remove
-        // if start inside, end not, update the one start is inside if end is larger
-        
-        for (int i = 0; i < freshIds2.size(); i++) {
-            long[] existingRange = freshIds2.get(i);
-            
-            if (startInsert > existingRange[1]) continue; // go to next range if our range is larger than whole
-
-            // we know we want to insert our range somewhere here. 
-
-            // check if we are smaller than start of this range as well:
-            boolean startInsertSmaller = startInsert < existingRange[0];
-
-            // need to check where our end is. 
-            for (int j = i; j < freshIds2.size(); j++) {
-                continue;
-            }
-
-
-            //  else, need to check how large the end is. 
-            //    if we end up being smaller than start, 
-            //          then we know we can set 1. start is insertstart, 2. end is insertend
-            //      and remove all ranges from i, (j-i) times
-            //          
-            //    if we end up being larger than a start, but smaller than end,
-            //          then we can update current to 1. start is insertstart, 2. end is end of where we ended up
-            
-        }
-
-        // no place to insert; insert at end
-        freshIds2.add(rangeToInsert);
     }
 }
